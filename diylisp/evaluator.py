49a2109488e4c5fdcbb01c939f553b4e4d55ea5a
nikolaia <nikolai@stasj.com>
2014-06-07 17:35:18 +0200

    Part 3 done

--- diylisp/evaluator.py
+++ diylisp/evaluator.py
@@ -19,6 +19,7 @@ def evaluate(ast, env):
 	
 	if (not isinstance(ast, list)): return ast
 	
+	if (ast[0] == "if"): return eval_if(ast, env)
 	if (ast[0] == "quote"): return ast[1]
 	elif (ast[0] == "atom"): return is_atom(ast,env)
 	elif (ast[0] == "eq"): return is_equal(ast,env)
@@ -30,41 +31,59 @@ def evaluate(ast, env):
 	elif (ast[0] == "mod"): return do_mod(ast, env)
 	elif (ast[0] == ">"): return do_greater(ast, env)
 
+def eval_if(ast, env):
+	arg1 = evaluate(ast[1], env)
+	if (arg1):
+		return evaluate(ast[2], env)
+	else: return evaluate(ast[3], env)
+
 def is_atom(ast, env):
-	if (isinstance(ast[1], list) and ast[1][0] == "quote"): 
-		return evaluate(["atom", ast[1][1]],env)
-	return evaluate(ast[1], env) == ast[1]
+	arg = evaluate(ast[1],env)
+	return not isinstance(arg, list)
 
 def is_equal(ast, env):
 	if (evaluate(["atom", ast[1]], env) and evaluate(["atom", ast[2]], env)):
-		return ast[1] == ast[2]
-	else: return False
+		return evaluate(ast[1], env) == evaluate(ast[2], env)
+	else: 
+		return False
 
-def is_number(number, env):
-	if (evaluate(number, env) == number and number is not True and number is not False):
+def is_number(number):
+	if (isinstance(number, int)):
 		return True
 	else: raise LispError("Not a number")
 
 def do_addition(ast, env):
-	if (is_number(ast[1], env) and is_number(ast[2], env)):
-		return ast[1] + ast[2]
+	arg1 = evaluate(ast[1], env)
+	arg2 = evaluate(ast[2], env)
+	if (is_number(arg1) and is_number(arg2)):
+		return arg1 + arg2
 
 def do_subtraction(ast, env):
-	if (is_number(ast[1], env) and is_number(ast[2], env)):
-		return ast[1] - ast[2]
+	arg1 = evaluate(ast[1], env)
+	arg2 = evaluate(ast[2], env)
+	if (is_number(arg1) and is_number(arg2)):
+		return arg1 - arg2
 
 def do_division(ast, env):
-	if (is_number(ast[1], env) and is_number(ast[2], env)):
-		return ast[1] / ast[2]
+	arg1 = evaluate(ast[1], env)
+	arg2 = evaluate(ast[2], env)
+	if (is_number(arg1) and is_number(arg2)):
+		return arg1 / arg2
 
 def do_multiplication(ast, env):
-	if (is_number(ast[1], env) and is_number(ast[2], env)):
-		return ast[1] * ast[2]
+	arg1 = evaluate(ast[1], env)
+	arg2 = evaluate(ast[2], env)
+	if (is_number(arg1) and is_number(arg2)):
+		return arg1 * arg2
 
 def do_mod(ast, env):
-	if (is_number(ast[1], env) and is_number(ast[2], env)):
-		return ast[1] % ast[2]
+	arg1 = evaluate(ast[1], env)
+	arg2 = evaluate(ast[2], env)
+	if (is_number(arg1) and is_number(arg2)):
+		return arg1 % arg2
 
 def do_greater(ast, env):
-	if (is_number(ast[1], env) and is_number(ast[2], env)):
-		return ast[1] > ast[2]
+	arg1 = evaluate(ast[1], env)
+	arg2 = evaluate(ast[2], env)
+	if (is_number(arg1) and is_number(arg2)):
+		return arg1 > arg2
